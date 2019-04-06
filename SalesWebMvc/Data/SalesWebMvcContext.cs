7f4ff7b2cb985bdee9c83f6894ea2d00128a3eca
Luiz Araujo <araujo.luiz23@gmail.com>
2019-04-06 20:13:12 -0300

    Other entities and second migrations

--- SalesWebMvc/Data/SalesWebMvcContext.cs
+++ SalesWebMvc/Data/SalesWebMvcContext.cs
@@ -13,6 +13,8 @@ namespace SalesWebMvc.Models
         {
         }
 
-        public DbSet<SalesWebMvc.Models.Departaments> Departaments { get; set; }
+        public DbSet<Departaments> Departaments { get; set; }
+        public DbSet<Seller> Seller { get; set; }
+        public DbSet<SalesRecord> SalesRecord { get; set; }
     }
 }
